1ef09ed1b121902955363d6402eae5cc90f754d9
Natanael Bezerra de Oliveira <natanbezerr63@gmail.com>
2022-05-28 11:50:04 -0400

    Primeira atividade

---
+++ primeira_atividade/hello.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Natanael Bezerra
+ * Date: 2022-05-28
+ * Program_name: Hello
+ * License: GNU
+ * */
+
+# include <stdio.h>
+
+void main(int arg, char**person){
+
+	if(arg == 2){
+		printf("Olá %s ao Mundo de C! \n", person[1]);
+	}
+}
+
